c944c9a4ab1e4f12d8a839cfe77a63ad76ae36fb
Mark <mark_klobukov@comcast.com>
2017-09-18 10:31:42 -0400

    Reorganize folder structure

---
+++ chronossdk.go
@@ -0,0 +1,187 @@
+package chronossdk
+
+import (
+  "fmt"
+  "bytes"
+  "encoding/json"
+  "encoding/base64"
+  "net/http"
+  "errors"
+  "io/ioutil"
+  "time"
+)
+
+type Config struct {
+  appkey            string    `json:"appkey"`
+  appsecret         string    `json:"appsecret"`
+  authManagerURL    string    `json:"authManagerURL"`
+  chronosURL        string    `json:"chronosURL"`
+  instanceID        string    `json:"instanceID"`
+  status            string    `json:status"`
+}
+
+
+func getAuthString(config *Config) (string) {
+  //var plainAuth = config.appkey + ":" + config.appsecret
+  var plainAuth = config.appkey + ":" + config.appsecret
+  authString := base64.URLEncoding.EncodeToString([]byte(plainAuth))
+  return authString
+}
+
+func getToken(config *Config) (string, error) {
+  payload := []byte(`{
+      "Type": "Server"
+  }`)
+
+  client := http.Client{
+    Timeout: time.Second * 2,
+  }
+
+  base64Auth := getAuthString(config)
+  req, err := http.NewRequest(http.MethodPost,
+                              config.authManagerURL + "/v1/login/",
+                              bytes.NewBuffer(payload))
+  if err != nil {
+    fmt.Println("Failed to create request: ", err.Error())
+    return "", err
+  }
+
+  req.Header.Set("Content-Type", "application/json")
+  req.Header.Set("Authorization", "Basic " + base64Auth)
+
+  res, getErr := client.Do(req)
+  if getErr != nil {
+    fmt.Println("Failed to execute request")
+    return "", getErr
+  }
+
+  if res.StatusCode != 200 {
+    return "", errors.New("Unauthorized")
+  }
+
+  body, readErr := ioutil.ReadAll(res.Body)
+  if readErr != nil {
+    fmt.Println("Failed to read response body")
+    return "", readErr
+  }
+
+  result := make(map[string]interface{})
+  err = json.Unmarshal(body, &result)
+  if err != nil {
+    return "", err
+  }
+
+  token, ok := result ["Token"].(string)
+  if !ok {
+    return "", errors.New("Invalid response type")
+  }
+
+  return token, nil
+}
+
+func updateJobStatus(config *Config) (string, error) {
+  //chronos currently doesn't return anything when status is updated
+  //the string return value can be used later if chronos responds with any data
+  payload := []byte(`{
+      "instance_id": "` + config.instanceID + `",
+      "status": "` + config.status + `"
+  }`)
+
+  client := http.Client{
+    Timeout: time.Second * 2,
+  }
+
+  token, err := getToken(config)
+  if err != nil {
+    fmt.Println("Failed to get token: ", err.Error())
+    return "", err
+  }
+
+  req, err := http.NewRequest(http.MethodPost,
+                              config.chronosURL + "/v1/jobcustomstatus",
+                              bytes.NewBuffer(payload))
+  if err != nil {
+    fmt.Println("Failed to create request: ", err.Error())
+    return "", err
+  }
+
+  req.Header.Set("Content-Type", "application/json")
+  req.Header.Set("Authorization", "Bearer " + token)
+
+  res, getErr := client.Do(req)
+  if getErr != nil {
+    fmt.Println("Failed to execute request")
+    return "", getErr
+  }
+
+  //check if error code is in 400s
+  if res.StatusCode/100 == 4 {
+    fmt.Println("Error updating job status: unauthorized")
+    return "", errors.New("Unauthorized")
+  }
+
+  body, readErr := ioutil.ReadAll(res.Body)
+  if readErr != nil {
+    fmt.Println("Failed to read response body")
+    return "", readErr
+  }
+
+  result := make(map[string]interface{})
+  err = json.Unmarshal(body, &result)
+  if err != nil {
+    return "", err
+  }
+
+  return "", nil
+}
+
+func getJobArgs(config *Config) (string, error) {
+  client := http.Client{
+    Timeout: time.Second * 2,
+  }
+
+  token, err := getToken(config)
+  if err != nil {
+    fmt.Println("Failed to get token: ", err.Error())
+    return "", err
+  }
+
+  getArgsURL := config.chronosURL + "/v1/getargs/instanceid/" + config.instanceID
+  req, err := http.NewRequest(http.MethodGet, getArgsURL, nil)
+  if err != nil {
+    fmt.Println("Failed to create request: ", err.Error())
+    return "", err
+  }
+  req.Header.Set("Content-Type", "application/json")
+  req.Header.Set("Authorization", "Bearer " + token)
+
+  res, getErr := client.Do(req)
+  if getErr != nil {
+    fmt.Println("Failed to execute request")
+    return "", getErr
+  }
+
+  //check if error code is in 400s
+  if res.StatusCode/100 == 4 {
+    fmt.Println("Error updating job status: unauthorized")
+    return "", errors.New("Unauthorized")
+  }
+
+  body, readErr := ioutil.ReadAll(res.Body)
+  if readErr != nil {
+    fmt.Println("Failed to read response body")
+    return "", readErr
+  }
+
+  result := make(map[string]interface{})
+  err = json.Unmarshal(body, &result)
+  if err != nil {
+    return "", err
+  }
+
+  args, ok := result["args"].(string)
+  if !ok {
+    return "", errors.New("Invalid response type")
+  }
+  return args, nil
+}
